5abbd669c53fe8b0c104668681a8ad8d42b7d048
CARIAGA, BRAIAN <bcariaga@santandertecnologia.com.ar>
2020-07-05 01:46:55 -0300

    ultimos cambios

--- www/html/php/person/insert.php
+++ www/html/php/person/insert.php
@@ -1,13 +1,16 @@
 <?php
+
 include("../database/connection.php");
 
 if ($conn->connect_error) {
     die("Fallo la conexión: " . $conn->connect_error);
 }
+
 $name = $_POST["name"];
 $surname = $_POST["surname"];
 $address_line1 = $_POST["address_line1"];
 $address_line2 = $_POST["address_line2"];
+
 $sql = "INSERT INTO ifts_21.Person(`name`, `surname`,`address_line1`, address_line2 ) VALUES ('$name', '$surname', '$address_line1', '$address_line2');";
 $success = false;
 $error = "";
